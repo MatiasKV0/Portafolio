8b3096db155f3fedb79d6906d183f60ddb11adf6
MatiasKV0 <159393104+MatiasKV0@users.noreply.github.com>
2024-03-08 19:48:59 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,28 +12,3 @@ btnSwitch.addEventListener("click",()=>{
     btnSwitch.classList.toggle("active");
 })
 
-window.addEventListener("scroll", function(){
-    let imagenes = document.querySelectorAll(".derecha");
-
-    imagenes.forEach(function(imagen){
-        let posicion = imagen.getBoundingClientRect().top;
-        let tamañoDePantalla = window.innerHeight;
-
-        if (posicion < tamañoDePantalla){
-            imagen.style.animation = "mover 1s ease-out";
-        }
-    });
-});
-
-window.addEventListener("scroll", function(){
-    let imagenes = document.querySelectorAll(".izquierda");
-
-    imagenes.forEach(function(imagen){
-        let posicion = imagen.getBoundingClientRect().top;
-        let tamañoDePantalla = window.innerHeight;
-
-        if (posicion < tamañoDePantalla){
-            imagen.style.animation = "moverinverso 1s ease-out";
-        }
-    });
-});
